c8ec6f14a4a56f9e9712342af6fc66db8c41084f
aph-96 <aylahanlon96@gmail.com>
2025-02-17 20:20:01 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function toggleCardContent(card) {
+  var extraContent = card.querySelector(".extra-content");
+  if (
+    extraContent.style.display === "none" ||
+    extraContent.style.display === ""
+  ) {
+    extraContent.style.display = "block"; // Show the additional content
+  } else {
+    extraContent.style.display = "none"; // Hide the additional content
+  }
+}
+function showTab(tabId) {
+  // Hide all content
+  document
+    .querySelectorAll(".content")
+    .forEach((el) => el.classList.remove("active"));
+  // Show selected tab
+  document.getElementById(tabId).classList.add("active");
+}
